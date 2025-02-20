d4c18a86deb41364e440ce1d23fbfb0f1a9881f8
Divvyanii <sdivyani2004@gmail.com>
2025-02-20 12:20:36 +0530

    changes

---
+++ server/src/utilis/csvReader.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const csv = require('csv-parser');
+const DataModel = require('../models/DataModel');
+const path = require('path');
+
+const csvFilePath = path.join(__dirname, '../../data/sample.csv');
+
+// Read and update MongoDB from CSV
+const readCSV = async () => {
+  const results = [];
+
+  fs.createReadStream(csvFilePath)
+    .pipe(csv({ headers: ['name', 'value', 'timestamp'], skipLines: 1 }))
+    .on('data', (data) => results.push(data))
+    .on('end', async () => {
+      await DataModel.deleteMany({}); // Clear old data
+      await DataModel.insertMany(results);
+      console.log('CSV data updated in the database');
+    });
+};
+
+// Watch for CSV changes
+const watchCSV = () => {
+  console.log(`Watching for changes in ${csvFilePath}`);
+  fs.watch(csvFilePath, (eventType) => {
+    if (eventType === 'change') {
+      console.log('CSV file changed, updating database...');
+      readCSV();
+    }
+  });
+};
+
+module.exports = watchCSV;
