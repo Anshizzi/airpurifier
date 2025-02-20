ee34f5aa81a64c8ca8c82e3b6a1a4525faccb5ba
Divvyanii <sdivyani2004@gmail.com>
2025-02-20 22:49:50 +0530

    5001

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const mongoose = require('./src/config/db');
 const watchCSV = require('./src/utilis/csvReader');
 const updateCSV = require('./src/utilis/updateCSV'); // Simulated data updates
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5001;
 
 // Watch the CSV file for real-time updates
 watchCSV();
