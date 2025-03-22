f15f67343c54166fcb5f5c3042ef273debf148ec
Divvyanii <sdivyani2004@gmail.com>
2025-03-22 10:02:13 +0530

    Your commit message

--- microservices/data-service/DataModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const DataSchema = new mongoose.Schema({
-  name: String,
-  value: Number,
-  timestamp: String,
-});
-
-module.exports = mongoose.model("SensorData", DataSchema);
--- microservices/data-service/dataRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getData } = require("../controllers/dataController");
-
-// ✅ API to fetch sensor data
-router.get("/data", getData);
-
-module.exports = router;
