658f9b05c973784a2bbddb3937074f0b41a1adf9
Divvyanii <sdivyani2004@gmail.com>
2025-03-22 19:54:09 +0530

    Your commit message

--- microservices/data-service/server.js
+++ microservices/data-service/server.js
@@ -16,7 +16,7 @@ mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopol
   .catch(err => console.error("❌ MongoDB connection error:", err));
 
 // Routes
-const dataRoutes = require("../dataroutes.js");
+const dataRoutes = require("./dataroutes");
 app.use("/", dataRoutes);
 
 // Start Server
